a7484685a2391c417965df5c2e7a29b3b2e98565
vasium <vasium@hotmail.com>
2025-02-23 20:23:35 +0100

    Add firebase package

--- Shwordle/ShwordleApp.swift
+++ Shwordle/ShwordleApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct ShwordleApp: App {
+    // Initialize Firebase
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
