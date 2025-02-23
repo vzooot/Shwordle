dff06913928aa5be0a036c9adab00150511b7e1e
vasium <vasium@hotmail.com>
2025-02-23 12:29:49 +0100

    Initial Commit

---
+++ Shwordle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shwordle
+//
+//  Created by Administrator on 2025-02-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shwordle/ShwordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShwordleApp.swift
+//  Shwordle
+//
+//  Created by Administrator on 2025-02-23.
+//
+
+import SwiftUI
+
+@main
+struct ShwordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
